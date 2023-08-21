b20cd73aba673bafe798334f97424da3c0f94dd5
Melek <melekk441@gmail.com>
2023-08-21 14:06:01 -0400

    git

---
+++ src/test/java/Homeworks/GitClass.java
@@ -0,0 +1,8 @@
+package Homeworks;
+
+public class GitClass {
+    public static void main(String[]args){
+        System.out.println("New branch");
+        System.out.println("New branch1");
+    }
+}
